59ce71eac7772f0fee0a84faa74a4a01f6807e69
jaimeprieto <jaime.prieto@alumnos.upm.es>
2024-09-23 20:45:36 +0200

    adding utils

--- src/main/java/upm/utils/PlayerPrinter.java
+++ src/main/java/upm/utils/PlayerPrinter.java
@@ -6,23 +6,8 @@ import java.util.List;
 
 public class PlayerPrinter implements Printer<Player>{
 
-    public static void printMenu(){
-        System.out.println("""
-                Choose an option:
-  
-                > create [player name]
-                > remove [player name]
-                > show
-                > rank
-                > score [player name];[score]
-                > show_matchmake
-                > clear_matchmake
-                > matchmake [player1];[player2]
-                > random_matchmake
-        """);
-    }
-
-    static void printEllements(List<Player> elements) {
+    @Override
+    public void printElements(List<Player> elements) {
         elements.forEach(System.out::println);
     }
 }
--- src/main/java/upm/utils/Printer.java
+++ src/main/java/upm/utils/Printer.java
@@ -3,6 +3,26 @@ package upm.utils;
 import java.util.List;
 
 public interface Printer<T> {
+    String MENU =
+        """
+            Choose an option:
+      
+            0 -> exit
+            1 -> create player
+            2 -> remove player
+            3 -> show
+            4 -> rank
+            5 -> score [player name];[score]
+            6 -> show_matchmake
+            7 -> clear_matchmake
+            8 -> matchmake [player1];[player2]
+            9 -> random_matchmake
+        """;
 
-    static <T> void printEllements(List<T> elements);
+    static void printMenu(){
+        System.out.println(MENU);
+    }
+
+    void printElements(List<T> elements);
+
 }
